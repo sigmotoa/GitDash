04d5e580121ffafb2182344d413a673f987b6470
sigmotoa <si.galvismotoa@gmail.com>
2026-02-18 06:40:22 -0500

    chore: bump versionCode to 5, versionName to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sigmotoa.gitdash"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "2"
+        versionCode = 5
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
